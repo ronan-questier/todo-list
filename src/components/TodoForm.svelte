fa83ef5f535dc18b4ec822175354cfa21269d9c7
questier-ronan <questier.ronan22@gmail.com>
2021-12-20 16:57:52 +0100

    Create TodoForm

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,16 @@
+<form action="submit" class="my-6">
+	<div class="flex flex-col text-sm mb-2">
+		<input
+			type="text"
+			name="todo"
+			id=""
+			placeholder="Nouvelle note..."
+			class="appearance-none shadow-sm border border-grey-200 p-2 focus:outlined-none focus:border-grey-500 rounded-lg"
+		/>
+	</div>
+	<button
+		type="submit"
+		class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+		>Ajouter</button
+	>
+</form>
