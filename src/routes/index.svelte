259cb40a116a042b6d31bb3084394a51c8d40b29
questier-ronan <questier.ronan22@gmail.com>
2021-12-20 16:57:37 +0100

    import TodoForm

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,8 @@
+<script>
+	import TodoForm from '../components/TodoForm.svelte';
+</script>
+
 <main>
 	<h1 class="text-2xl font-bold text-center text-grey-800 md:text-3xl">Mes tâches</h1>
+	<TodoForm />
 </main>
