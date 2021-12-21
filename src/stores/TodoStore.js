354122fa3a1397f176a388ff74d92aa55de1a281
questier-ronan <questier.ronan22@gmail.com>
2021-12-21 12:22:17 +0100

    fix bug in toggleTodoCompleted

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -24,13 +24,14 @@ export const toggleTodoCompleted = (id) => {
 	todos.update((todos) => {
 		let index = -1;
 		for (let i = 0; i < todos.length; i++) {
-			if (todos[i].id == id) {
+			if (todos[i].id === id) {
 				index = i;
 				break;
 			}
 		}
-		if (index != -1) {
+		if (index !== -1) {
 			todos[index].completed = !todos[index].completed;
 		}
+		return todos;
 	});
 };
